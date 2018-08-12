d1dabf73108a22c123ef786819c665c332c6bc41
Luis Kupferberg <luchotex.kuper.android@gmail.com>
2018-08-12 01:04:22 -0400

    -- Refactoring to multimodule maven project. Closes #1.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,4 +1,4 @@
-package gur.springfrawork.sfgpetclinic;
+package guru.springframework.sfgpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,4 +1,4 @@
-package gur.springfrawork.sfgpetclinic;
+package sfgpetclinic;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
