6f61c7c7d67f6ad086b3139c3b300b642b0c75e6
Luis Kupferberg <luchotex.kuper.android@gmail.com>
2018-08-13 22:02:11 -0400

    -- Adding Owners page and controller. Closes #14 (this is due to an rror).

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Luis
+ * on 13/08/2018.
+ */
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners(){
+        return "owners/index";
+    }
+}
