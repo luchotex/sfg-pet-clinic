1fffa45e838599b34ebbb282a1a81778e0d94d70
Luis Kupferberg <luchotex.kuper.android@gmail.com>
2018-10-01 21:53:28 -0400

    Added Visits, update Owner with pet relationship. Closes #27.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,8 +1,20 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by Luis
  * on 11/08/2018.
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
