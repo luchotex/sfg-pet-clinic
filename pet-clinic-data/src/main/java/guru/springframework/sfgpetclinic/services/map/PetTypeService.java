9a0cf7383eb550b2a880d8ba8174c2b4b812f073
Luis Kupferberg <luchotex.kuper.android@gmail.com>
2018-10-02 23:33:28 -0400

    Added PetTypeService. Closes #30.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetTypeService.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.services.CrudService;
+
+/**
+ * Created by Luis
+ * on 02/10/2018.
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
