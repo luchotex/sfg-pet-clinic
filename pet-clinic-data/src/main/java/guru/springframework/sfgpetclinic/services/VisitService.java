e9c3e5d37abe186a4f5603cc70f547567621a964
Luis Kupferberg <luchotex.kuper.android@gmail.com>
2018-10-09 21:48:11 -0400

    Implemented Map Impl Visit Service Test. Closes #47.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+
+/**
+ * Created by Luis
+ * on 09/10/2018.
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
