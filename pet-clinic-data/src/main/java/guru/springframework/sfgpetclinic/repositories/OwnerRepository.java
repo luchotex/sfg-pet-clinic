8b43bfb2a321cb1cd422e902fcc86b9338bd68f0
Luis Kupferberg <luchotex.kuper.android@gmail.com>
2018-11-06 19:31:03 -0400

    Impl find Owner. Closes #55

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,6 +3,8 @@ package guru.springframework.sfgpetclinic.repositories;
 import guru.springframework.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by Luis
  * on 08/10/2018.
@@ -10,4 +12,7 @@ import org.springframework.data.repository.CrudRepository;
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
+
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -2,6 +2,8 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Owner;
 
+import java.util.List;
+
 /**
  * Created by Luis
  * on 12/08/2018.
@@ -9,4 +11,6 @@ import guru.springframework.sfgpetclinic.model.Owner;
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
